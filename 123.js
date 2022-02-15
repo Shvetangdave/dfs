877abfe773bfcaf6d3319a81b98752769e4f8729
Devarsh <87245954+Shvetangdave@users.noreply.github.com>
2022-02-15 15:40:48 -0500

    Add files via upload

---
+++ 123.js
@@ -0,0 +1,7 @@
+var Score1 = 0;
+
+function n9(){
+  document.getElementById("9n").innerHTML='Corerect';
+    
+    
+}
---
+++ intro.js
@@ -0,0 +1,16 @@
+
+
+function numbers(){
+    window.location="123.html";
+
+}
+
+function alpha(){
+    window.location="abc.html";
+
+}
+
+function colors(){
+    window.location="color.html";
+}
+    
